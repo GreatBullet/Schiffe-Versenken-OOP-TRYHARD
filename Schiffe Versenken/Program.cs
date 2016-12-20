9609957bc452e55ce2708e148a2c53323a78d8c0
Rick <Rick@DESKTOP-6BB2TGB>
2016-12-20 23:03:18 +0100

    Projektdateien hinzufügen.

---
+++ Schiffe Versenken/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schiffe_Versenken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3;i++)
+            {
+                if (Ships.battleship._bcnt > 0)
+                {
+                    Ships.battleship B = new Ships.battleship(22, 44, 55, 66);
+                    Console.WriteLine("funkt");
+                }
+                else
+                {
+                    Console.WriteLine(Ships.battleship._bcnt);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ships/Ships.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ships
+{
+    public class battleship
+    {
+        int BugY, BugX;
+        int HeckY, HeckX;
+        string Shipdef = "bat";
+        int Length = 5;
+        static int bcnt = 1;
+        public battleship(int BugY, int BugX, int HeckY, int HeckX)
+        {
+            --bcnt;
+            this.BugX = BugX;
+            this.BugY = BugY;
+            this.HeckX = HeckX;
+            this.HeckY = HeckY;
+        }
+        public static int _bcnt
+        {
+            get{ return bcnt; }
+        }
+    }
+
+    public class cruiser
+    {
+        int BugY, BugX;
+        int HeckY, HeckX;
+        string Shipdef = "cru";
+        int Length = 4;
+        static int ccnt = 2;
+        public cruiser(int BugY, int BugX, int HeckY, int HeckX)
+        {
+            --ccnt;
+            this.BugX = BugX;
+            this.BugY = BugY;
+            this.HeckX = HeckX;
+            this.HeckY = HeckY;
+        }
+    }
+
+    public class destroyer 
+    {
+        int BugY, BugX;
+        int HeckY, HeckX;
+        string Shipdef = "des";
+        int Length = 3;
+        static int dcnt = 3;
+        public destroyer(int BugY, int BugX, int HeckY, int HeckX)
+        {
+            --dcnt;
+            this.BugX = BugX;
+            this.BugY = BugY;
+            this.HeckX = HeckX;
+            this.HeckY = HeckY;
+        }
+    }
+
+    public class submarine
+    {
+
+        int BugY, BugX;
+        int HeckY, HeckX;
+        string Shipdef = "sub";
+        int Length = 2;
+        static int scnt = 4;
+
+        public submarine(int BugY, int BugX, int HeckY, int HeckX)
+        {
+            --scnt;
+            this.BugX = BugX;
+            this.BugY = BugY;
+            this.HeckX = HeckX;
+            this.HeckY = HeckY;
+        }
+    }
+}
